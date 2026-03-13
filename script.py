0f00520daab2e87f93c3d011652803766146cf98
Михаил <nicksylive@gmail.com>
2026-03-13 19:38:18 +0300

    third step

--- script.py
+++ script.py
@@ -22,10 +22,33 @@ canvas.pack()
 
 snake = [(100, 100), (90, 100), (80, 100)]
 direction = 'Right'
-food = None
 score = 0
 game_over = False
 
+def create_food():
+    while True:
+        x = random.randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+        y = random.randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+        if (x, y) not in snake:
+            return (x, y)
 
+def draw_food():
+    canvas.create_rectangle(
+        food[0], food[1],
+        food[0] + CELL_SIZE,
+        food[1] + CELL_SIZE,
+        fill = 'red'
+    )
+
+def game_loop():
+    global snake, food, score
+
+    canvas.delete('all')
+    draw_food()
+    root.after(DELAY, game_loop)
+
+food = create_food()
+draw_food()
+root.after(DELAY, game_loop)
 
 root.mainloop()
