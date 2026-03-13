46c3490807ed096f04b53d00aa389b0cb01ef24b
Михаил <nicksylive@gmail.com>
2026-03-13 20:37:41 +0300

    fifth step

--- script.py
+++ script.py
@@ -73,15 +73,53 @@ def move_snake():
         new_head = (head_x + CELL_SIZE, head_y)
 
     snake.insert(0, new_head)
-    snake.pop()
+    if not check_food_collision():
+        snake.pop()
+
+def check_food_collision():
+    global food, score
+    if snake[0] == food:
+        score+=1
+        food = create_food()
+        return True
+    return False
+
+def update_title():
+    root.title(f"Змейка | Счет: {score}")
+
+def check_wall_collision():
+    head_x, head_y = snake[0]
+    return (
+        head_x < 0 or head_x >= WIDTH or
+        head_y < 0 or head_y >= HEIGHT
+    )
+
+def end_game():
+    global game_over
+    game_over = True
+    canvas.create_text(
+        WIDTH // 2, HEIGHT // 2,
+        text=f"Game Over! Score: {score}",
+        fill = 'white',
+        font=('Arial', 24)
+    )
 
 def game_loop():
     global snake, food, score
 
+    if game_over:
+        return
+
     move_snake()
+
+    if check_wall_collision():
+        end_game()
+        return
+
     canvas.delete('all')
     draw_food()
     draw_snake()
+    update_title()
     root.after(DELAY, game_loop)
 
 
