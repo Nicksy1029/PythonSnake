3a4f2fe5d6433a93cbda1fd7152456086e92f4ad
Михаил <nicksylive@gmail.com>
2026-03-13 19:21:43 +0300

    second step

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+import random
 
 WIDTH = 400
 HEIGHT = 400
+DIRECTIONS = ['Up', 'Down', 'Left', 'Right']
+CELL_SIZE = 10
+DELAY = 100
 
 root = tk.Tk()
 root.title('Змейка | Счет: 0')
@@ -15,5 +19,13 @@ canvas = tk.Canvas(
     highlightthickness = 0
 )
 canvas.pack()
+
+snake = [(100, 100), (90, 100), (80, 100)]
+direction = 'Right'
+food = None
+score = 0
+game_over = False
+
+
 
 root.mainloop()
